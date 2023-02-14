ee0949390fe69177deac128b8cb3d671043ff2d5
Woullocke <125007309+Woullocke@users.noreply.github.com>
2023-02-14 10:44:04 +0300

    Update main.cpp

--- Строки/Task15/Task15/main.cpp
+++ Строки/Task15/Task15/main.cpp
@@ -7,7 +7,7 @@ int main() {
     std::string str;
     std::getline(std::cin, str);
 
-    if (str.length() > 10) {
+    if (str.length() >= 10) {
         str = str.substr(0, 6);
     } else {
         str.append(12 - str.length(), 'o');
